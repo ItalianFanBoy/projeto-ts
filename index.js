9560372e9c461e91c31eaa4468787c592e0e6c64
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-05-23 22:02:03 -0300

    Projetinho Finalizado
    Projetinho finalizado

--- index.js
+++ index.js
@@ -5,9 +5,40 @@ function main() {
     var firstStr = (0, readline_sync_1.question)('Enter firt number:\n');
     var operator = (0, readline_sync_1.question)('Enter operator\n');
     var secondStr = (0, readline_sync_1.question)('Enter second number\n');
-    console.log(firstStr, operator, secondStr);
-    var firstNum = isnumber(firstStr);
-    console.log(firstNum);
+    var validInput = isnumber(firstStr) && isoperator(operator) && isnumber(secondStr);
+    if (validInput) {
+        var firstNum = parseInt(firstStr);
+        var secondNum = parseInt(secondStr);
+        var result = calculate(firstNum, operator, secondNum);
+        console.log(result);
+    }
+    else {
+        console.log('\ninvalid Input\n');
+        main();
+    }
+}
+function calculate(firstNum, operator, secondNum) {
+    switch (operator) {
+        case '+':
+            return firstNum + secondNum;
+        case '-':
+            return firstNum - secondNum;
+        case '/':
+            return firstNum / secondNum;
+        case '*':
+            return firstNum * secondNum;
+    }
+}
+function isoperator(operator) {
+    switch (operator) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true;
+        default:
+            return false;
+    }
 }
 function isnumber(str) {
     var maybeNum = parseInt(str); //33 NaN
--- index.ts
+++ index.ts
@@ -1,15 +1,53 @@
 import{question} from 'readline-sync';
 
+type Operator = '+' | '-' | '*' | '/';
+
 function main():void {
     const firstStr: string = question('Enter firt number:\n');
     const operator: string = question('Enter operator\n');
     const secondStr: string = question('Enter second number\n');
 
-    console.log(firstStr, operator, secondStr)
+    const validInput: boolean = isnumber(firstStr) && isoperator(operator) && isnumber(secondStr);
+    if (validInput){
+        const firstNum:number = parseInt(firstStr);
+        const secondNum: number = parseInt(secondStr);
+        const result = calculate(firstNum, operator as Operator, secondNum);
+        console.log(result);
 
-    const firstNum = isnumber(firstStr)
-    console.log(firstNum)
+    }
+    else{
+        console.log('\ninvalid Input\n');
+        main()
+    }
 }
+
+    function calculate(firstNum:number, operator:Operator, secondNum:number)
+    {
+        switch(operator){
+            case '+':
+                return firstNum + secondNum;
+            case '-':
+                return firstNum - secondNum;
+            case '/':
+                return firstNum / secondNum;
+            case '*':
+                return firstNum * secondNum;
+
+        }
+    }
+
+    function isoperator(operator:string):boolean{
+        switch(operator){
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                return true;
+            
+               default: 
+               return false;
+        }
+    }
 
 function isnumber(str: string):boolean {
     const maybeNum = parseInt(str); //33 NaN
