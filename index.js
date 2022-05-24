5584b1c8d1044ca7dcaf42057cd26e73062de802
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-05-23 21:27:59 -0300

    brincando com ts e node

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("eu estou falando");
+"use strict";
+exports.__esModule = true;
+var readline_sync_1 = require("readline-sync");
+function main() {
+    var firstStr = (0, readline_sync_1.question)('Enter firt number:\n');
+    var operator = (0, readline_sync_1.question)('Enter operator\n');
+    var secondStr = (0, readline_sync_1.question)('Enter second number\n');
+    console.log(firstStr, operator, secondStr);
+    var firstNum = isnumber(firstStr);
+    console.log(firstNum);
+}
+function isnumber(str) {
+    var maybeNum = parseInt(str); //33 NaN
+    var isnum = !isNaN(maybeNum);
+    return isnum;
+}
+main();
--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("eu estou falando")
+import{question} from 'readline-sync';
+
+function main():void {
+    const firstStr: string = question('Enter firt number:\n');
+    const operator: string = question('Enter operator\n');
+    const secondStr: string = question('Enter second number\n');
+
+    console.log(firstStr, operator, secondStr)
+
+    const firstNum = isnumber(firstStr)
+    console.log(firstNum)
+}
+
+function isnumber(str: string):boolean {
+    const maybeNum = parseInt(str); //33 NaN
+    const isnum:boolean = !isNaN(maybeNum);
+    return isnum;
+}
+
+main();
